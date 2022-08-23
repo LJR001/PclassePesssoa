486d4a3b98935e899587280fd4d7e59bcb190d3e
Luciano <junior.romano2001@gmail.com>
2022-08-23 19:50:41 -0300

    Adicionar arquivos de projeto.

---
+++ PclassePesssoa/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PclassePesssoa
+{
+    internal class Pessoa
+    {
+        String nome;
+        int data_nasc;
+        int cpf;
+        int telefone;
+
+        public
+    }
+}
---
+++ PclassePesssoa/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PclassePesssoa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            Pessoa pessoa2 = new Pessoa();
+            Pessoa pessoa3 = new Pessoa();
+        }
+    }
+}
