4bfdad250a25ba8082edf89d3400e41c6f868057
Luciano <junior.romano2001@gmail.com>
2022-08-23 23:04:39 -0300

    Atualizacao grafica

--- PclassePesssoa/Program.cs
+++ PclassePesssoa/Program.cs
@@ -9,11 +9,14 @@ namespace PclassePesssoa
            
             String[,] Cadastro = new String[3, 4];
 
-            Console.WriteLine("******** Cadastro de usuario *******\n");
+            Console.WriteLine("******** Cadastrar de usuario *******\n");
             
             Cadastros();
+            Console.Clear();
 
             ImprimirDados();
+
+            
 
             void Cadastros()
             {
@@ -25,22 +28,22 @@ namespace PclassePesssoa
                         switch (coluna)
                         {
                             case 0:
-                                Console.WriteLine("Digite o Nome");
+                                Console.Write("Digite   o   nome: ");
                                 Cadastro[linhas, coluna] = Console.ReadLine();
                                 //  Console.ReadKey();
                                 break;
                             case 1:
-                                Console.WriteLine("Digite o data");
+                                Console.Write("Digite   a   data: ");
                                 Cadastro[linhas, coluna] = Console.ReadLine();
                                 //  Console.ReadKey();
                                 break;
                             case 2:
-                                Console.WriteLine("Digite o cpf");
+                                Console.Write("Digite   o    CPF: ");
                                 Cadastro[linhas, coluna] = Console.ReadLine();
                                 //  Console.ReadKey();
                                 break;
                             case 3:
-                                Console.WriteLine("Digite o telefone");
+                                Console.Write("Digite o telefone: ");
                                 Cadastro[linhas, coluna] = Console.ReadLine();
                                 Console.WriteLine();
                                 //  Console.ReadKey();
@@ -58,10 +61,13 @@ namespace PclassePesssoa
 
             void ImprimirDados()
             {
+                Console.WriteLine("******** Cadastro dos usuarios *******\n");
                 for (int j = 0; j < 3; j++)
                 {
+                    Console.WriteLine();
                     Pessoa pessoa = new Pessoa(Cadastro[j, 0], Cadastro[j, 1], Cadastro[j, 2], Cadastro[j,3]);
                     Console.WriteLine(pessoa.ToString());
+                    Console.WriteLine();
                 }
             }
             
